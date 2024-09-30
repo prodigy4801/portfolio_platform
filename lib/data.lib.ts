477ba9c660f1d9ca454f0437071429e2abae00b8
Samuel Esezobor <samuel.esezobor076@gmail.com>
2024-09-30 05:44:50 +0100

    commit layout setup and header component review

---
+++ lib/data.lib.ts
@@ -0,0 +1,36 @@
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { FaReact } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+import corpcomment from '@/public/corpcomment.png';
+import rmtdev from '@/public/rmtdev.png';
+import wordanalytics from '@/public/wordanalytics.png';
+
+import { TNavigationLinkModel } from '@/models/lib.model';
+
+export const NavigationLink: TNavigationLinkModel[] = [
+  {
+    name: 'Home',
+    hash: '#home',
+  },
+  {
+    name: 'About',
+    hash: '#about',
+  },
+  {
+    name: 'Projects',
+    hash: '#projects',
+  },
+  {
+    name: 'Skills',
+    hash: '#skills',
+  },
+  {
+    name: 'Experience',
+    hash: '#experience',
+  },
+  {
+    name: 'Contact',
+    hash: '#contact',
+  },
+] as const;
---
+++ models/lib.model.ts
@@ -0,0 +1,4 @@
+export type TNavigationLinkModel = {
+  name: string;
+  hash: string;
+};
